fcc66bff163d98e85cb6da18723321aa76d7ed8e
Jason McGinty <jason_mcginty@yahoo.com>
2021-10-12 20:47:17 -0500

    adjusting Services photo styling

--- bybee-mfg/src/components/services/Services.js
+++ bybee-mfg/src/components/services/Services.js
@@ -9,6 +9,7 @@ import './Services.css';
 const overview = {
     title: 'Overview',
     text: ['Utilizing advanced technology, JD Machine achieves an amazingly high level of component parts throughput for medium- to high-complexity production runs. We mainly serve the aerospace, defense, medical, energy and commercial industries, and our capabilities include precision CNC machining, sheet metal fabrication, assembly, testing and more. We engineer our processes offline using powerful CAD/CAM and CNC program simulation software with up-to-the-minute tracking of OEE and throughput on every machine. The combination of the latest technologies and our highly trained, skilled and motivated team ensures world-class quality at extremely competitive pricing.'],
+    // text: ['Yeethole'],
     photo: overviewPhoto
 };
 
